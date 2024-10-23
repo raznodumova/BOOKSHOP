81f9ba9063cfe22637a595e79429121dd5ac0d42
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-10-23 15:34:25 +0300

    пока как-то так

--- BOOKSHOP/bookstore/models.py
+++ BOOKSHOP/bookstore/models.py
@@ -12,7 +12,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    count = models.IntegerField()
+    count = models.PositiveIntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
--- BOOKSHOP/bookstore/views.py
+++ BOOKSHOP/bookstore/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -5,7 +6,7 @@ from rest_framework import status
 
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
-from django.db import transaction
+
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -17,17 +18,13 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    @action(detail=True, methods=['post'])
+    @transaction.atomic
+    @action(detail=True, methods=['post'], queryset=Book.objects.selector_for_update())
     def buy(self, request, pk=None):
         book = self.get_object()
+        book.count -= 1
         try:
-            with transaction.atomic():
-                if book.count > 0:
-                    book.count -= 1
-                    book.save()
-                    return Response({'message': 'Книга куплена :)'})
-                else:
-                    return Response({'message': 'Книги закончилась, выбери другую :('}, status=status.HTTP_400_BAD_REQUEST) # иногда я развлекаюсь с сообщениями
-
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            book.save()
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
