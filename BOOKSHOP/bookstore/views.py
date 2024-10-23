49c011e898dc024e6b03e67b2909fafd9f4dd60a
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-10-23 10:57:04 +0300

    modified views

--- BOOKSHOP/bookstore/views.py
+++ BOOKSHOP/bookstore/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
+from django.db import transaction
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -18,9 +20,14 @@ class BookViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'])
     def buy(self, request, pk=None):
         book = self.get_object()
-        if book.count > 0:
-            book.count -= 1
-            book.save()
-            return Response({'message': 'Book purchased successfully!'})
-        else:
-            return Response({'error': 'Book is out of stock!'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            with transaction.atomic():
+                if book.count > 0:
+                    book.count -= 1
+                    book.save()
+                    return Response({'message': 'Книга куплена :)'})
+                else:
+                    return Response({'message': 'Книги закончилась, выбери другую :('}, status=status.HTTP_400_BAD_REQUEST) # иногда я развлекаюсь с сообщениями
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
