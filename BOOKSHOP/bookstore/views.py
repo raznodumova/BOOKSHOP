6f5b294311c608ce1b08e459bed66890c6f2c192
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-10-28 10:20:06 +0300

    все еще как-то так

--- BOOKSHOP/bookstore/views.py
+++ BOOKSHOP/bookstore/views.py
@@ -8,7 +8,6 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 
 
-
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
